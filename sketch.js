4a09d62391212997c20230136ac8913189769f3e
Cametendo <cameronmathis08@gmail.com>
2026-03-30 11:41:01 +0200

    Added interactivity

--- sketch.js
+++ sketch.js
@@ -3,15 +3,23 @@ let r, g, b = 0;
 function setup() {
   createCanvas(800, 800);
   strokeWeight(5);
-  frameRate(5)
-  background(100, 100, 100, 1);
-  for(i = 0; i < 10; i++) {
-    stroke(random(255), random(255), random(255))
-    noFill()
-    square(random(width), random(height), random(300));
-  }
+
+  
 }
 
 function draw() {
-  frameRate(5);
+  frameRate()
+  background(100, 100, 100, 100);
+  for(i = 0; i < 1; i++) {
+    stroke(random(255), random(255), random(255))
+    noFill()
+    const rectWidth = random(300);
+    const rectHeight = random(300);
+    const rectMidX = (width/2) - (rectWidth/2);
+    const rectMidY = (height/2) - (rectHeight/2);
+    const offsetX = random(-40, 40);
+    const offsetY = random(-40, 40);
+    rect(rectMidX + offsetX, rectMidY + offsetY, rectWidth, rectHeight);
+    rect(mouseX + offsetX, mouseY + offsetY, rectWidth, rectHeight);
+  }
 }
