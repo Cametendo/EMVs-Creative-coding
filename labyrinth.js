adc8b47772479ab04728a3913b2ca1a239721d98
Cametendo <cameronmathis08@gmail.com>
2026-04-01 10:31:11 +0200

    Add shapes

--- labyrinth.js
+++ labyrinth.js
@@ -1,8 +1,17 @@
+let x, y = 0;
+
 function setup() {
-  
+  createCanvas(800, 800);
+  strokeWeight(5); 
+  background("gray");
 }
 
 
 function draw() {
-
+  for (let x = 0; x <= width; x += 40) {
+    for (let y = 0; y <= height; y += 40) {
+        square(x, y, 40);
+    }
+  }
+  line(width, height, -800, -800); // :3
 }
